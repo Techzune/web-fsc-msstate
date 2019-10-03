d1dffe74669fda5b3895e3f84356290291d89df3
Jordan Stremming <jordan@jordanstremming.com>
2019-10-02 20:51:23 -0500

    Contact-us page design

--- contact.php
+++ contact.php
@@ -4,33 +4,31 @@ include "inc/baseHTML.php";
 ?>
 
 <?php startblock("content") ?>
+
 <div class="text-center justify-content-center">
-  <div class="card w-50 mx-auto my-2">
-    <div class="card-body">
-      <h3 class="card-title">Call us!</h3>
-      <div class="card-text">
-        662-325-7698
-      </div>
-    </div>
+
+  <div class="col-lg-9 mx-auto">
+    <img src="/img/map_herzer.png" class="img-fluid" alt="Map of Herzer"/>
   </div>
-  <div class="card w-50 mx-auto my-2">
-    <div class="card-body">
-      <h3 class="card-title">Email us!</h3>
+
+  <div class="col-lg-6 mx-auto mt-4">
+    <div class="card card-body">
+      <h3 class="card-title">Call us</h3>
+      <p class="card-text">662-325-7698</p>
+
+      <h3 class="card-title mt-3">Email us</h3>
+      <p class="card-text">MSUFoodSciClub@gmail.com</p>
+
+      <h3 class="card-title mt-3">Mail us</h3>
       <div class="card-text">
-        MSUFoodSciClub@gmail.com
-      </div>
-    </div>
-  </div>
-  <div class="card w-50 mx-auto my-2">
-    <div class="card-body">
-      <h3 class="card-title">Mail us!</h3>
-      <div class="card-text">
-        <b>Food Science Club</b><br/>
+        <i>Food Science Club</i><br/>
         945 Stone Boulevard<br/>
         Box 9808<br/>
         Mississippi State, MS 39762
       </div>
     </div>
   </div>
+
 </div>
+
 <?php endblock() ?>
